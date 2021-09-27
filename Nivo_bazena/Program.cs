fae3d220d1bfb4b311be13f284a0cfc6d1146bf8
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-27 11:48:18 +0200

    Add files via upload

---
+++ Nivo_bazena/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nivo_bazena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, a, b, c;
+            n = double.Parse(Console.ReadLine());
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(n / 10);
+        }
+    }
+}
